a213c88e467b3bf3b8821c440b0c5d83e84eb71f
SimpllleUser <pecheritsa.vv@gmail.com>
2022-04-19 12:57:02 +0300

    fix: eslint warning

--- src/helper/project-template-helper.ts
+++ src/helper/project-template-helper.ts
@@ -1,9 +1,11 @@
-import { copySourceDirectoryToDirectory, removeDirectory } from "./storage-helper";
+import {
+  copySourceDirectoryToDirectory,
+  removeDirectory,
+} from './storage-helper';
 const projectConfigPath = './files/project-config';
 const projectsPath = './files/projects';
 
 export const createProjectTemplateByName = async (name) => {
-  // Copy source body file by projectConfigPath to id directory;
   return await copySourceDirectoryToDirectory(
     projectConfigPath,
     `${projectsPath}/${name}`,
